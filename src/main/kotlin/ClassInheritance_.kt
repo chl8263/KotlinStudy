9633e704f94cfaba94fa117aa39d247cc431ba64
ChoiWonGyun (Ewan) <chl8263@naver.com>
2020-04-14 10:21:49 -0700

    Class inheritance

--- src/main/kotlin/ClassInheritance_.kt
+++ src/main/kotlin/ClassInheritance_.kt
@@ -1,7 +1,9 @@
 class ClassInheritance_ {
     fun run(){
-        Thing("Ewan")
-        Thing2("Ewan", "Seattle")
+//        Thing("Ewan")
+//        Thing2("Ewan", "Seattle")
+        val car: Car = BrokenCar("Pony")
+        car.speed = 100
     }
 }
 
@@ -22,4 +24,17 @@ class Thing2 (val name: String){
     constructor(name: String, address: String, nick: String) : this(name) {
 
     }
+}
+
+abstract class Car {
+    abstract val name : String
+    open var speed: Int = 0
+}
+
+class BrokenCar(override val name: String) : Car(){
+    override var speed: Int
+        get() = 0
+        set(value){
+            throw UnsupportedOperationException("This car is broken")
+        }
 }
